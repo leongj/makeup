dbd8f767c5a57c6b6d28c9385e5de2be74347e79
thivy <thivyakanth@gmail.com>
2024-12-09 10:19:56 +1100

    feat: simplify alt text prompt by removing examples for clarity

--- features/product-page/alt-text/prompt.ts
+++ features/product-page/alt-text/prompt.ts
@@ -3,11 +3,4 @@ export const AltTextSystemPrompt = `You are an AI system designed to generate de
 1. Accuracy: Describe the content of the image as precisely as possible without making assumptions about details that are unclear or subjective.
 2. Clarity: Use simple and descriptive language that conveys the image's key visual details in just 1 sentence and maximum of 20 words.
 3. Product and Subjects: Identify and describe prominent subject of the image and not the scene behind it.
-
-## **Examples**
-- For an image of a dog playing with a ball in a park:  
-  *"A brown Labrador Retriever playing with a red ball on green grass in a sunny park."*
-- For a decorative abstract pattern:  
-  *"An abstract design with swirling blue and gold lines on a black background."*
-
 `;
