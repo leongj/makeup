1baf793a81dc97588778fbcc4fb8d585af43a50a
Jason Leong <jasonleong@microsoft.com>
2025-05-12 15:29:03 +1000

    new icon and colours

--- features/product-page/header.tsx
+++ features/product-page/header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
     <div className="py-4 shadow-2xl shadow-slate-300/25 flex gap-6">
       <motion.img
         initial={{ opacity: 0, y: -10, scale: 0.8, rotate: 0 }}
-        animate={{ opacity: 1, y: 0, scale: 1, rotate: -22 }}
+        animate={{ opacity: 1, y: 0, scale: 1, rotate: 45 }} // Changed rotate to 45
         src="/icon-192x192.png"
         className="size-14 inline-block"
       />
@@ -50,21 +50,21 @@ export const LandingPage = () => {
   }
 
   return (
-    <div className="items-center justify-center flex gap-6">
+    <div className="items-center justify-center flex gap-0">
       <motion.img
         initial={{ opacity: 0, y: -10, scale: 0.8, rotate: 0 }}
-        animate={{ opacity: 1, y: 0, scale: [1.5, 1], rotate: -22 }}
+        animate={{ opacity: 1, y: 0, scale: [1.7, 1], rotate: 20 }} // Changed rotate to 45
         transition={{ delay: 0.2 }}
-        src="/icon-192x192.png"
-        className="size-20 inline-block"
+        src="/icon-crop.png"
+        className="size-24 inline-block object-contain"
       />
       <div className="flex gap-3 flex-col text-balance">
         <motion.h1
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
-          className="flex gap-4 items-center col-start-2 col-span-10 text-6xl font-semibold bg-gradient-to-r from-violet-500  to-blue-500 bg-clip-text text-transparent"
+          className="flex gap-4 items-center col-start-2 col-span-10 text-6xl font-semibold bg-gradient-to-r from-red-500 to-red-800 bg-clip-text text-transparent" // Changed to red gradient
         >
-          <span> Lipstick</span>
+          <span>Lipstick</span>
         </motion.h1>
         <motion.h1
           initial={{ opacity: 0, y: -10 }}
@@ -72,8 +72,8 @@ export const LandingPage = () => {
           transition={{ delay: 0.2 }}
           className="text-xl "
         >
-          Demo powered by{" "}
-          <span className="font-semibold bg-gradient-to-r from-violet-500  to-blue-500 bg-clip-text text-transparent">
+          Powered by{" "}
+          <span className="font-semibold bg-gradient-to-r from-red-500 to-red-800 bg-clip-text text-transparent">
             Microsoft AI
           </span>
         </motion.h1>
@@ -98,12 +98,12 @@ export const Links = () => {
           animate={{ opacity: 1, y: 0 }}
           href={link.link}
           key={link.name}
-          className="group p-2 px-4 bg-white hover:bg-violet-100 hover:text-violet-500 transition-all shadow-lg rounded-full border-violet-600/45 border-2 text-sm flex gap-2 items-center"
+          className="group p-2 px-4 bg-white hover:bg-red-100 hover:text-red-500 transition-all shadow-lg rounded-full border-red-600/45 border-2 text-sm flex gap-2 items-center"
         >
           {link.name}
           <ArrowRight
             size={16}
-            className="text-violet-600 transform group-hover:scale-125 transition-transform duration-200"
+            className="text-red-600 transform group-hover:scale-125 transition-transform duration-200"
           />
         </MotionLink>
       ))}
