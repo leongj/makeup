af3377715d924b3dc3f421017320b3c1c086f0d1
Jason Leong <jasonleong@microsoft.com>
2025-05-13 09:04:51 +1000

    prompting

--- features/product-page/product-description/prompt.ts
+++ features/product-page/product-description/prompt.ts
@@ -6,15 +6,15 @@ export const RecommendationSystemPrompt =
  `You are an AI Assistant that helps user with lipstick recommendations.
   Respond to the user in the first person.
   You're provided with 2 images
-  1. A photo that captures their skin tone
+  1. A selfie of the user.
   2. A swatch image of lipstick shades for the Charlotte Tilbury lipstick "Hot Lips".
   They are dressing for the following occasion: '{occasion}'.
 
   Based on this information recommend a suitable lipstick shade.
 
   EXAMPLE FORMAT:
-  I can see you have an X skin tone.
+  I can see you have an (X) skin tone and (Y) hair.
 
-  From Charlotte Tilbury's "Hot Lips" collection, I recommend the shade Y for you, because <reason>.
+  From Charlotte Tilbury's "Hot Lips" collection, I recommend the shade (A) and (B) for you, because <reason>.
   `;
   
